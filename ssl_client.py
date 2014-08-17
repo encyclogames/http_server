1ff63c3714049574b507c891b78ba25abf0b2197
Encyclogames <encyclogames@gmail.com>
2014-08-18 00:11:55 +0300

    added a POST test

--- ssl_client.py
+++ ssl_client.py
@@ -50,4 +50,31 @@ pprint.pprint(tls.recv(4096).split("\r\n"))
 pprint.pprint(tls.recv(4096))
 tls.close()
 
+
+
+sock = socket.create_connection(('localhost', 4443))
+#tls = ssl.wrap_socket(sock, cert_reqs=ssl.CERT_REQUIRED,
+#                            ca_certs='../certs/signer.crt',
+#                            ssl_version=ssl.PROTOCOL_TLSv1)
+tls = ssl.wrap_socket(sock, cert_reqs=ssl.CERT_REQUIRED,
+                            ca_certs='./myca.crt',
+                            ssl_version=ssl.PROTOCOL_TLSv1)
+tls.sendall('POST /www/cgi/collect.cgi HTTP/1.1\r\n'
+'Host: 127.0.0.1:4443\r\n'
+'Connection: keep-alive\r\n'
+'Content-Length: 29\r\n'
+'Cache-Control: max-age=0\r\n'
+'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n'
+'Origin: https://127.0.0.1:4443\r\n'
+'User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/36.0.1985.125 Chrome/36.0.1985.125 Safari/537.36\r\n'
+'Content-Type: application/x-www-form-urlencoded\r\n'
+'Referer: https://127.0.0.1:4443/\r\n'
+'Accept-Encoding: gzip,deflate,sdch\r\n'
+'Accept-Language: en-US,en;q=0.8\r\n'
+'\r\n'
+'data=Testing+you+chromium+ssl')
+pprint.pprint(tls.recv(4096).split("\r\n"))
+pprint.pprint(tls.recv(4096))
+tls.close()
+
 exit(0)
